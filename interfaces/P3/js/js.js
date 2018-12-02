7b70fb93552173e42849fc1253bf3ff45e1b4a0e
ach74 <32370243+ach74@users.noreply.github.com>
2018-12-02 04:08:11 +0100

    interfaces

--- interfaces/P3/js/js.js
+++ interfaces/P3/js/js.js
@@ -1,15 +1,23 @@
 window.onload = function() {
 
-window.onscroll = function() {myFunction()};
+	window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("containerMenu");
-var sticky = navbar.offsetTop;
+	var navbar = document.getElementById("containerMenu");
+	var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			navbar.classList.add("sticky")
+		} else {
+			navbar.classList.remove("sticky");
+		}
+	}
 };
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
