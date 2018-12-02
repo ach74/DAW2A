1aefb4f907dd07a8ab0fdb4a1ab1248b90f719d3
ach74 <32370243+ach74@users.noreply.github.com>
2018-12-02 22:10:07 +0100

    akhjdgbaskj;ghb

--- interfaces/P3/js/js.js
+++ interfaces/P3/js/js.js
@@ -12,12 +12,36 @@ window.onload = function() {
 			navbar.classList.remove("sticky");
 		}
 	}
+	
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight){
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
 };
 
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+	document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+	document.getElementById("myNav").style.width = "0%";
+}
+
+function openMenu() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
 }
