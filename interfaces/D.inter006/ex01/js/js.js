76fdc9cbdbdabc94586cda290cd8da9fb2dc76be
ach74 <32370243+ach74@users.noreply.github.com>
2018-12-14 01:10:38 +0100

    teminar

---
+++ interfaces/D.inter006/ex01/js/js.js
@@ -0,0 +1,22 @@
+
+
+window.onload = function() {
+
+
+	/*var issue = document.querySelector("#inputText-B");
+	issue.addEventListener("input", function(){this.setCustomValidity("");},false);
+	issue.addEventListener("activate", function(){this.setCustomValidity("");},false);
+	issue.addEventListener("invalid", function(){this.setCustomValidity("D\355ganos tu Issue");},false);
+
+	var email = document.querySelector("#inputText-E");
+	email.addEventListener("input", function(){this.setCustomValidity("");},false);
+	email.addEventListener("activate", function(){this.setCustomValidity("");},false);
+	email.addEventListener("invalid", function(){this.setCustomValidity("D\355ganos tu email");},false);
+*/
+	alert("ssj");
+
+    var porTagName=document.getElementsByTagName("input")[2].value;
+
+    alert(var);
+
+};
